ab169f3e04820c321e93274fc32d9d1e6f1a3189
Zelusiek <51979332+Zelusiek@users.noreply.github.com>
2019-10-15 19:49:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,15 +5,18 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
+/**
+* Multiple message events for every command can cause data over-flows.
+*/
 
 client.on('message', message => {
+    //Add only user commands can only in guild
+    if(!message.guild || message.user.bot) return;
     if (message.content === 'bing') {
     	message.reply('BONG!');
+  	}
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
   	}
 });
 
